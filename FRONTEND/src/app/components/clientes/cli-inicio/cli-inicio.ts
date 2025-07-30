6e4574d1156595789d81c0f59d8c434e3efe3d3f
nycatino <nycatino@hotmail.com>
2025-07-29 23:38:30 -0300

    Se agrega funcionalidad del boton ver historial y se arregla el html de la componente historial

--- FRONTEND/src/app/components/clientes/cli-inicio/cli-inicio.ts
+++ FRONTEND/src/app/components/clientes/cli-inicio/cli-inicio.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { VehiculoService } from '../../../services/vehiculo/vehiculo.service'
 import {Vehiculo} from '../../../models/vehiculo/vehiculo.model'
 
+import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -14,7 +15,7 @@ import { CommonModule } from '@angular/common';
 export class CliInicio implements OnInit {
   vehiculos: Vehiculo[] = []
   
-  constructor(private vehiculoService: VehiculoService) {}
+  constructor(private vehiculoService: VehiculoService, private router: Router) {}
 
   ngOnInit(): void {
     this.vehiculoService.getVehiculos().subscribe(data => this.vehiculos = data);
@@ -22,11 +23,20 @@ export class CliInicio implements OnInit {
 
   administrarPermisos(vehiculo: Vehiculo) {
     console.log(`Permisos para: ${vehiculo.patente}`);
+  //  this.router.navigate(['/permisos']);
   }
 
   verHistorial(vehiculo: Vehiculo) {
     console.log(`Historial de: ${vehiculo.patente}`);
+    this.router.navigate(['/cliente', 'historial', vehiculo.id]);
   }
 
+  proximoMantenimiento(vehiculo: Vehiculo) {
+
+  }
+
+  kilometraje(vehiculo: Vehiculo) {
+    
+  }
 
 }
