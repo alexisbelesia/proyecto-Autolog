639d530231cef2038f49f938d1327a057b7c04bf
Bernardo <bernardobasaldua@gmail.com>
2025-07-31 15:03:15 -0300

    Se agregaron los modelos de las interfaces FRONTEND restantes

--- FRONTEND/src/app/components/clientes/cli-turnos/cli-turnos.ts
+++ FRONTEND/src/app/components/clientes/cli-turnos/cli-turnos.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cli-turnos',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './cli-turnos.html',
   styleUrl: './cli-turnos.css'
 })
 export class CliTurnos {
+  activeTab = signal<'turnos' | 'sugerido'>('turnos');
 
+  setTab(tab: 'turnos' | 'sugerido') {
+    this.activeTab.set(tab);
+  }
 }
--- FRONTEND/src/app/components/compartido/header/header.ts
+++ FRONTEND/src/app/components/compartido/header/header.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -8,5 +10,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.css']
 })
 export class Header {
+  private router = inject(Router);
+  //Signal para guardar el título actual
+  currentTitle = signal<string>('Inicio');
 
+  constructor() {
+    this.router.events
+    .pipe(filter(event => event instanceof NavigationEnd))
+    .subscribe((event: NavigationEnd) => {
+      const path = event.urlAfterRedirects;
+      this.currentTitle.set(this.mapPathToTitle(path));
+    })
+  }
+  mapPathToTitle(path: string): string {
+    //Rutas a título de clientes
+    if (path.startsWith('cliente')) return 'Inicio';
+    if (path.startsWith('cliente/historial')) return 'Historial';
+    if (path.startsWith('cliente/turnos')) return 'Turnos';
+    if (path.startsWith('cliente/talleres')) return 'Talleres';
+    if (path.startsWith('cliente/config')) return 'Configuración';
+
+    //Rutas a título de talleres
+    if (path.startsWith('taller/ordenes')) return 'Órdenes de trabajo';
+    if (path.startsWith('taller/clientes')) return 'Clientes';
+    if (path.startsWith('taller/vehiculos')) return 'Vehículos';
+    if (path.startsWith('taller/turnos')) return 'Agenda de turnos';
+    if (path.startsWith('taller/config')) return 'Configuración';
+
+    return 'Inicio'; // Título por defecto
+  }
 }
